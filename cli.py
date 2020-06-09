2e693f3f16bcd93f12892a684fbdefef9f036f2f
jungee <jungebah@gmail.com>
2020-06-09 15:20:56 -0700

    added checks for project

--- cli.py
+++ cli.py
@@ -1,6 +1,8 @@
 import click
 import configparser
 import os
+import sys
+import yaml
 
 def get_info(key):
     homde_dir = os.path.expanduser('~')
@@ -16,6 +18,24 @@ def get_project():
 
 def get_api():
     return get_info('api')
+
+def does_contain(folder_name: str, required_folder: str):
+    all_folder = [x for x in os.listdir(folder_name) 
+                    if os.path.isdir(os.path.join(folder_name,x))]
+    if required_folder in all_folder:
+        return True
+    return False
+
+def check_setup():
+    homde_dir = os.path.expanduser('~')
+    if os.path.isfile(os.path.join(homde_dir,'.daamiReview')):
+        return True
+    return False
+
+def folder_Present(dir_name):
+    if os.path.isdir(dir_name):
+        return True
+    return False
 
 @click.group(help='Will help to aid in creating files for daamireview')
 def cli():
@@ -40,6 +60,23 @@ def setup(project,api_key):
 @click.option('--title','-T', prompt=True, help='title of the article')
 @click.option('--movie','-m', prompt=True,help='Name of the movie being reviewed')
 @click.option('--time','-t',help='When the article is being written')
+
 def review(title,movie,time,project):
     ''' Generates the review file in the _post folder of the project'''
-    print('YUP')
+    # check if setup file is there or not before proceeding
+    if not check_setup():
+        click.echo(click.style('Cannot find config file do run daami-cli setup', fg='red'))
+        sys.exit(1)
+    # get the project folder
+    project_foler = get_project()
+    api_key = get_api()
+    # checks to make sure the project folder is present
+    if not folder_Present(project_foler):
+        click.echo(click.style('The project folder specified in setup is not present', fg='red'))
+        sys.exit(1)
+    # checks to make sure the project folder has _posts
+    if not does_contain(project_foler,'_posts'):
+        click.echo(click.style('The project folder doesn\'t look like a jekyll project', fg='red'))
+        sys.exit(1)
+
+        
