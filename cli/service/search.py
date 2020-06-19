4ba077609961773badcb311fce0fba3c0082e7fd
jungee <jungebah@gmail.com>
2020-06-19 00:23:44 -0700

    major changes to remove errors

--- cli/service/search.py
+++ cli/service/search.py
@@ -1,5 +1,5 @@
 from googleapiclient.discovery import build
-from PyInquirer import prompt, print_json
+from PyInquirer import prompt
 import re
 import os
 import requests
@@ -11,39 +11,35 @@ from bs4 import BeautifulSoup
 class Search:
     """Extending the search functionality for movie info"""
 
-    def __init__(
-        self, movie_name, api_key, movie_language, review_template, project_folder
-    ):
-        self.movie_name = movie_name
-        self.movie_language = movie_language
+    def __init__(self, api_key, review_template):
         self.review_template = review_template
         self.search_service = build("customsearch", "v1", developerKey=api_key)
         self.cx = "008985690000369000569:gswsgp4ncli"
-        self.project_folder = project_folder
 
-    def _imdb_search(self):
+    def _imdb_search(self, movie_name, movie_language):
         """search the custom search engine with imdb"""
         return (
             self.search_service.cse()
-            .list(q=f"{self.movie_name} {self.movie_language}", cx=self.cx,)
+            .list(q=f"{movie_name} {movie_language}", cx=self.cx,)
             .execute()
         )
 
-    def _check_name_correction(self, data):
+    def _check_name_correction(self, data, name):
         if "spelling" in data:
-            corrected_query = data["spelling"]["correctedQuery"].replace(self.movie_language, "")
+            corrected_query = data["spelling"]["correctedQuery"].replace(
+                self.review_template["language"], ""
+            )
             correct_name = [
                 {
                     "type": "confirm",
                     "name": "changeMovieName",
-                    "message": f"Instead of {self.movie_name} are you looking for {corrected_query}",
+                    "message": f"Instead of {name} are you looking for {corrected_query}",
                     "default": False,
                 }
             ]
             answers = prompt(correct_name)
             if answers:
-                return corrected_query.strip()
-        return self.movie_name
+                self.review_template["movie_name"] = corrected_query.strip()
 
     def _filter_result(self, data):
         movie_result = [
@@ -52,12 +48,11 @@ class Search:
                 "name": "movie_name",
                 "message": "Identify the name from the IMDb list",
                 "choices": [],
-                # 'filter': lambda val: val.lower()
             }
         ]
         info = {}
         for item in data["items"]:
-            if re.match(r'.*\s*\([0-9]{4}\)\s*\-\s*IMDb', item["title"]):
+            if re.match(r".*\s*\([0-9]{4}\)\s*\-\s*IMDb", item["title"]):
                 movie_result[0]["choices"].append(item["title"])
                 info[item["title"]] = item
         # add an option for if things don't match
@@ -65,15 +60,14 @@ class Search:
         result = prompt(movie_result)
         if result["movie_name"] == "other":
             click.echo(click.style("Nothing from IMDb to use", fg="green"))
-            return None
+            return False
         else:
-            name = re.sub(r'\s*\([0-9]{4}\)\s*\-\s*IMDb','',result["movie_name"])
-            if name.lower() != self.movie_name.lower():
-                self.movie_name = name
+            name = re.sub(r"\s*\([0-9]{4}\)\s*\-\s*IMDb", "", result["movie_name"])
+            if name.lower() != self.review_template["movie_name"].lower():
+                self.review_template["movie_name"] = name
             return info[result["movie_name"]]
-            # template['movie_name'] = re.sub(r'\([0-9]{4}\)\s*\-\s*imdb','',result['movie_name']).title()
 
-    def _image_download(self, image_url):
+    def _image_useable(self, image_url):
         if "imdb_fb_logo" in image_url:
             return False
         return True
@@ -82,26 +76,37 @@ class Search:
         movie_number = movie_id[2:]
         imdb_instance = IMDb()
         movie = imdb_instance.get_movie(movie_number)
+        movie_info = {}
+        if "certificates" in movie:
+            american_certificate = [
+                x
+                for x in movie["certificates"]
+                if "United States" in x and "TV" not in x
+            ]
+            if american_certificate:
+                movie_info["rating"] = american_certificate[0].replace("United States:", "")
         cast_crew = {}
-        cast_crew["starring"] = [x["name"] for x in movie["cast"][:11]]
+        cast_crew["starring"] = [x["name"] for x in movie["cast"][:5]]
         cast_crew["director"] = [x["name"] for x in movie["directors"][:11]]
         cast_crew["producer"] = [x["name"] for x in movie["producers"][:11]]
-        movie_info = {}
         movie_info["genre"] = [x for x in movie["genres"]]
-        length = movie["runtimes"][0]
-        movie_info["length"] = f"{length} mins"
-        return movie_info, cast_crew
+        if 'runtimes' in movie:
+            length = movie["runtimes"][0]
+            movie_info["length"] = f"{length} mins"
+        self.review_template["movie"] = movie_info
+        self.review_template["cast-crew"] = cast_crew
 
     def data(self):
-        data = self._imdb_search()
-        self.movie_name = self._check_name_correction(data)
+        data = self._imdb_search(
+            self.review_template["movie_name"], self.review_template["language"]
+        )
+        self._check_name_correction(data, self.review_template["movie_name"])
         movie_data = self._filter_result(data)
         if movie_data:
             image_url = movie_data["pagemap"]["metatags"][0]["og:image"]
-            movie_info, cast_info = self._crawl_imdb_info(
-                movie_data["pagemap"]["metatags"][0]["pageid"]
-            )
-            if "og:image" in movie_data["pagemap"]["metatags"][0] and self._image_download(image_url):
-                return self.movie_name,image_url, movie_info, cast_info,movie_data["pagemap"]["metatags"][0]["pageid"]
-            return self.movie_name,False, movie_info, cast_info,movie_data["pagemap"]["metatags"][0]["pageid"]
-        return self.movie_name,False,None,None,None
+            self._crawl_imdb_info(movie_data["pagemap"]["metatags"][0]["pageid"])
+            if "og:image" in movie_data["pagemap"]["metatags"][
+                0
+            ] and self._image_useable(image_url):
+                self.review_template["image"] = image_url
+        return self.review_template, movie_data["pagemap"]["metatags"][0]["pageid"]
--- cli/service/tm.py
+++ cli/service/tm.py
@@ -28,46 +28,56 @@ def get_tmdb():
 
 
 class Tm:
-    def __init__(self, movie,imdb_id):
+    def __init__(self, template, imdb_id):
         self.tmdb = tmdb
         self.tmdb.API_KEY = get_tmdb()
-        self.movie = movie
+        self.template = template
         self.imdb_id = imdb_id
-
 
     def _search(self):
         search_string = self.tmdb.Search()
-        movie_search = search_string.movie(query=self.movie)
-        return search_string, movie_search
-        
-    def _get_trailer(self,id):
-        url = f'http://api.themoviedb.org/3/movie/{id}/videos?api_key={self.tmdb.API_KEY}'
+        movie_search = search_string.movie(query=self.template["movie_name"])
+        return search_string
+
+    def _get_trailer(self, id):
+        url = (
+            f"http://api.themoviedb.org/3/movie/{id}/videos?api_key={self.tmdb.API_KEY}"
+        )
         result = requests.get(url).content
         dict_result = result.decode("UTF-8")
         info = ast.literal_eval(dict_result)
-        for video_list in info['results']:
-            if video_list and 'Trailer' in video_list['name']:
-                video_id = video_list['key']
-                return f'https://www.youtube.com/watch?v={video_id}'
+        for video_list in info["results"]:
+            if video_list:
+                if "Trailer" in video_list["name"] or 'Promo' in video_list['name']:
+                    video_id = video_list["key"]
+                    return f"https://www.youtube.com/watch?v={video_id}"
         return None
 
-
     def get_info(self):
-        search_string, response = self._search()
+        search_string = self._search()
         movie_id = []
         movie_info = {}
+        trailer = ""
+        image_url = None
         for r in search_string.results:
             data = self._movie_info(r["id"])
             if data["imdb_id"] == self.imdb_id:
                 if self._get_trailer(r["id"]):
-                    movie_info['trailer'] = self._get_trailer(r["id"])
-                poster = data['poster_path']
-                image_url = f'https://image.tmdb.org/t/p/original{poster}'
-                movie_info["genres"] = [x["name"] for x in data["genres"]]
-                movie_info["runtime"] = data["runtime"]
-                movie_info["imdb_id"] = data["imdb_id"]
-                return image_url,movie_info
-        return False,{}
+                    trailer = self._get_trailer(r["id"])
+                poster = data["poster_path"]
+                if poster:
+                    image_url = f"https://image.tmdb.org/t/p/original{poster}"
+                for k in self.template["movie"]:
+                    if "rating" in k:
+                        movie_info["rating"] = k["rating"]
+                movie_info["genre"] = [x["name"] for x in data["genres"]]
+                run = data["runtime"]
+                movie_info["length"] = f"{run} mins"
+                if movie_info:
+                    self.template["movie"] = movie_info
+                if image_url:
+                    self.template["image"] = image_url
+        return self.template, trailer
 
     def _movie_info(self, id):
         movie = self.tmdb.Movies(id)
